8ad00f0a76857d277f23b8dc3e48647567eb99ff
Dima <Dima@DEVIDE>
2022-09-09 15:48:16 +0300

    Добавьте файлы проекта.

---
+++ Weather_Bot/DataBase.cs
@@ -0,0 +1,38 @@
+﻿using Parsering;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace NewAbhWeatherBot
+{
+    public class DataBase
+    {
+        public static void AddToDB(string url, ref string city, ref string temp, ref string feels, ref string wind, ref string humidity, ref string pressure, ref string water)
+        {
+            Parser.Parse(url, ref city, ref temp, ref feels, ref wind, ref humidity, ref pressure, ref water);
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Parser"].ConnectionString);
+            string query = "INSERT INTO AbhWeather (City, Temp, Feels, Wind, Humidity, Pressure, Water) VALUES (@City, @Temp, @Feels, @Wind, @Humidity, @Pressure, @Water)";
+            SqlCommand command = new SqlCommand(query, con);
+            command.Parameters.AddWithValue("@City", city);
+            command.Parameters.AddWithValue("@Temp", temp);
+            command.Parameters.AddWithValue("@Feels", feels);
+            command.Parameters.AddWithValue("@Wind", wind);
+            command.Parameters.AddWithValue("@Humidity", humidity);
+            command.Parameters.AddWithValue("@Pressure", pressure);
+            command.Parameters.AddWithValue("@Water", water);
+            try
+            {
+                con.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Произошла ошибка при передаче данных в базу. Код ошибки: " + e.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+            return;
+        }
+    }
+}
---
+++ Weather_Bot/Parser.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsering
+{
+    public class Parser
+    {
+        public static void Parse(string url, ref string city, ref string temp, ref string feels, ref string wind, ref string humidity, ref string pressure, ref string water)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+            city = doc.DocumentNode.SelectNodes("/html/body/div[2]/div[2]/h2").First().InnerText;
+            temp = doc.DocumentNode.SelectNodes("/html/body/div[2]/div[2]/div[2]/div/div[1]/div[2]/span[2]/text()").First().InnerText;
+            feels = doc.DocumentNode.SelectNodes("/html/body/div[2]/div[2]/div[2]/div/div[1]/div[2]/span[4]").First().InnerText;
+            wind = doc.DocumentNode.SelectNodes("/html/body/div[2]/div[2]/div[2]/div/div[1]/div[2]/span[8]").First().InnerText;
+            humidity = doc.DocumentNode.SelectNodes("/html/body/div[2]/div[2]/div[2]/div/div[1]/div[2]/span[7]").First().InnerText;
+            pressure = doc.DocumentNode.SelectNodes("/html/body/div[2]/div[2]/div[2]/div/div[1]/div[2]/span[6]").First().InnerText;
+            water = doc.DocumentNode.SelectNodes("/html/body/div[2]/div[2]/div[2]/div/div[1]/div[2]/span[5]").First().InnerText;
+        }
+    }
+}
